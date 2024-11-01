81d74bc740f0c85d1118ce3c66f0d4b95a27a379
dDeeDev <dee.hamdee.084@gmail.com>
2024-11-01 12:07:44 +0700

    feat(script): :tada: helper for convert address

--- typescript/client.ts
+++ typescript/client.ts
@@ -15,3 +15,31 @@ if (!process.env.TEST_ACCOUNT) {
 }
 
 export const wallet = new Wallet(process.env.TEST_ACCOUNT, provider);
+
+
+export async function getConnectorConfig(
+  network: string,
+): Promise<{ rpcUrl: string; apiUrl: string; mnemonic: string }> {
+  switch (network) {
+    case "local":
+      return {
+        rpcUrl: "http://localhost:26657",
+        apiUrl: "http://localhost:1317",
+        mnemonic: process.env.ALICE_MNEMONIC!,
+      };
+    case "fivenet":
+      return {
+        rpcUrl: "https://rpc1.fivenet.sixprotocol.net:443",
+        apiUrl: "https://api1.fivenet.sixprotocol.net:443",
+        mnemonic: process.env.TECHSAUCE_TESTNET_MNEMONIC!,
+      };
+    case "sixnet":
+      return {
+        rpcUrl: "https://sixnet-rpc.sixprotocol.net:443",
+        apiUrl: "https://sixnet-api.sixprotocol.net:443",
+        mnemonic: process.env.TECHSAUCE_MNEMONIC!,
+      };
+    default:
+      throw new Error("Invalid network");
+  }
+}
